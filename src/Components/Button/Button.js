a9e93afc29ba718b139f5d19e1cd59de3bbbbc59
Zain Quteshat <zain.quteshat@gmail.com>
2021-04-21 15:51:45 +0200

    Added styling to the components of the landing page

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,4 +1,9 @@
-// const Button = (props) => {
-//     return <button>{props.title}</button>
-// }
-// export default Button;
+import classes from "./Button.module.scss";
+const Button = (props) => {
+  return (
+    <button className={`${classes.Button} ${classes.Active}`}>
+      {props.title}
+    </button>
+  );
+};
+export default Button;
--- src/Container/Container.js
+++ src/Container/Container.js
@@ -2,16 +2,19 @@ import classes from "./Container.module.scss";
 import AddItem from "../Components/AddItem/AddItem";
 import TaskList from "../Components/TaskList/TaskList";
 import Button from "../Components/Button/Button";
+
 const Container = () => {
   return (
     <div className={classes.Container}>
       <AddItem className={classes.AddItem} />
       <div className={classes.SeparatingLine}></div>
       <TaskList className={classes.TaskList} />
-      {/*<div className="SeparatingLine"></div> 
-       <Button title="ALL" />
-      <Button title="OPEN" />
-      <Button title="COMPLETED" /> */}
+      <div className={classes.SeparatingLine}></div>
+      <div className={classes.Buttons}>
+        <Button title="ALL" />
+        <Button title="OPEN" />
+        <Button title="COMPLETED" />
+      </div>
     </div>
   );
 };
